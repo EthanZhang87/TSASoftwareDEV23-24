e2f896c3970eab839de90033722c3dad9ab5f25e
IconicKeyboard <71947274+EthanZhang87@users.noreply.github.com>
2023-10-24 21:28:53 -0400

    .

--- Answerly/tutor/admin.py
+++ Answerly/tutor/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Tutor, Message
+from .models import Tutor
 # Register your models here.
 
 admin.site.register(Tutor)
-admin.site.register(Message)
+
